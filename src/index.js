9480b87d030b8f7833e4bbbe949cfb86e77268fe
nick-456 <94133116+nick-456@users.noreply.github.com>
2021-11-14 09:44:40 +0900

    a

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
  * テンプレート文字列
  */
 
-const name = "ジャケえ";
-const age = 28;
+// const name = "ジャケえ";
+// const age = 28;
 // 「私の名前はジャケえです。年齢は28歳です。」
 
 // 従来の方法
@@ -11,5 +11,28 @@ const age = 28;
 // console.log(message);
 
 // テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+//  従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(4, 7));
